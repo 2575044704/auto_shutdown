ae0e5b1bd05a245ab023b0e8b94cdaf55cfa1083
2575044704 <2575044704@qq.com>
2024-05-06 19:09:17 +0800

    Add files via upload

--- shutdown.sh
+++ shutdown.sh
@@ -2,7 +2,7 @@
 
 while true; do
     # 获取 PID 为 8 的进程运行时长
-    runtime=$(ps -o etime= -p 8)
+    runtime=$(ps -o etime= -p 20)
 
     # 解析运行时长中的分钟部分
     minutes=$(echo $runtime | awk -F: '{print $1}')
@@ -10,7 +10,8 @@ while true; do
     # 判断运行时长是否大于 29 分钟
     if [ $minutes -gt 28 ]; then
         echo "Process with PID 8 has been running for more than 29 minutes. Killing the process..."
-        kill 8
+        # 获取除了PID为80以外的所有进程的PID，并关闭它们
+        ps aux | awk '$2 != 80 {print $2}' | grep -v 'PID' | xargs kill -9
     else
         echo "Process with PID 8 is still running within the 29-minute limit."
     fi
