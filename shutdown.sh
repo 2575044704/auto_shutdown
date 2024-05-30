a0cc7a552d65c748c39711f7650a5cd03119f271
2575044704 <2575044704@qq.com>
2024-05-30 21:34:22 +0800

    Update shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 获取 Jupyter Lab 进程的 PID
 pid=$(pgrep -f jupyter-lab)
-
+echo "获取 Jupyter Lab 进程的 PID"
 while true; do
     # 获取 PID 为 20 的进程运行时长
     runtime=$(ps -o etime= -p $pid)
@@ -11,11 +11,12 @@ while true; do
     echo $minutes
     # 判断运行时长是否大于 29 分钟
     if [ $minutes -gt 28 ]; then
-        echo "Process with PID 8 has been running for more than 29 minutes. Killing the process..."
+        echo "运行时长已超过29分钟，将于10秒内关机，注意保存数据！！"
+        sleep 10
         # 获取除了PID为80以外的所有进程的PID，并关闭它们
         ps aux | awk '$2 != 80 {print $2}' | grep -v 'PID' | xargs kill -9
     else
-        echo "Process with PID 8 is still running within the 29-minute limit."
+        echo "运行时长仍在29分钟以下，不关机"
     fi
-    sleep 50
+    sleep 40
 done
